d00c7283d6a90a8685ef2ca706b27edf6f1fcb57
priscilla <prissever@gmail.com>
2015-01-24 17:53:53 -0200

    Inspectores

--- app/src/main/java/com/example/priscilla/inspectores/verificacionLogin.java
+++ app/src/main/java/com/example/priscilla/inspectores/verificacionLogin.java
@@ -1,42 +1,25 @@
 package com.example.priscilla.inspectores;
 
-import java.math.BigInteger;
-import java.security.SecureRandom;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
 
-public class verificacionLogin extends Activity{
+public class verificacionLogin extends Activity {
     String user, pass;
-    private static final String SERVICE_URL = "http://192.168.1.47:8081/RestWebService/rest/person";
-    private static final String TAG = "verificacionLogin";
+    String userResponse;
+    String passResponse;
+    boolean resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,287 +34,93 @@ public class verificacionLogin extends Activity{
         user = dataUser[0];
         pass = dataUser[1];
 
+        WebServiceTask wst = new WebServiceTask();
+        wst.execute(user);
+
+    }
+
+    private class WebServiceTask extends AsyncTask<String, Integer, Boolean> {
+
+        private static final String SERVICE_URL = "http://192.168.1.46:8081/RestWebService/rest/person";
+        private static final String TAG = "verificacionLogin";
         String sampleURL = SERVICE_URL + "/demo";
+        private ProgressDialog progressDialog=null;
 
-        //WebServiceTask wst = new WebServiceTask(WebServiceTask.GET_TASK, this, "GETting data...");
+        @Override
+        protected Boolean doInBackground(String... params) {
+            HttpClient httpClient = new DefaultHttpClient();
 
-        //wst.execute(new String[] { sampleURL });
+            HttpGet del = new HttpGet(sampleURL);
+            del.setHeader("content-type", "application/json");
 
-        //String response = wst.doInBackground(sampleURL);
-        //System.out.println(response);
-        HttpClient httpClient = new DefaultHttpClient();
+            try {
+                publishProgress();
+                HttpResponse resp = httpClient.execute(del);
+                String respStr = EntityUtils.toString(resp.getEntity());
+                JSONObject respJSON = new JSONObject(respStr);
 
-        HttpGet del = new HttpGet(sampleURL);
-        del.setHeader("content-type", "application/json");
+                userResponse = respJSON.getString("userName");
+                passResponse = respJSON.getString("txtPass");
 
-        try
-        {
-            HttpResponse resp = httpClient.execute(del);
-            String respStr = EntityUtils.toString(resp.getEntity());
+                if (user.equals(userResponse) && pass.equals(passResponse)) {
+                    //Genero Token para mantener la sesión.
+                    //String token;
+                    //SessionIdentifierGenerator sessionId = new SessionIdentifierGenerator();
+                    //token = sessionId.nextSessionId();
+                    //Toast.makeText(verificacionLogin.this, token, Toast.LENGTH_LONG).show();
 
-            JSONObject respJSON = new JSONObject(respStr);
+                    resultado=true;
 
-            String userResponse = respJSON.getString("userName");
-            String passResponse = respJSON.getString("txtPass");
-            if (user.equals(userResponse) && pass.equals(passResponse)) {
 
-                //Genero Token para mantener la sesión.
-                //String token;
-                //SessionIdentifierGenerator sessionId = new SessionIdentifierGenerator();
-                //token = sessionId.nextSessionId();
-                //Toast.makeText(verificacionLogin.this, token, Toast.LENGTH_LONG).show();
+                } else {
+
+                    resultado= false;
+
+                }
+
+            } catch (Exception ex) {
+                Log.e("ServicioRest", "Error!", ex);
+                resultado = false;
+            }
+            return resultado;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(verificacionLogin.this);
+            progressDialog.setCancelable(false);
+            progressDialog.setIndeterminate(true);
+
+        }
+
+
+        @Override
+        protected void onProgressUpdate(Integer... progress){
+            super.onProgressUpdate(progress);
+
+            progressDialog.show(verificacionLogin.this,"wait","Iniciando sesión");
+
+        }
+
+
+        protected void onPostExecute(Boolean result){
+            progressDialog.dismiss();
+            if (result){
                 Intent returnIntent = new Intent();
                 //returnIntent.putExtra("tokenSession", token);
                 returnIntent.putExtra("userLoged", user);
                 setResult(RESULT_OK, returnIntent);
                 finish();
-
-            } else {
+            }
+            else{
                 Intent returnIntent = new Intent();
                 setResult(RESULT_CANCELED, returnIntent);
                 finish();
-
             }
 
-
-        }
-        catch(Exception ex)
-        {
-            Log.e("ServicioRest","Error!", ex);
-            Intent returnIntent = new Intent();
-            setResult(RESULT_CANCELED, returnIntent);
-            finish();
-        }
-        }
-
-
-
-/*
-    public void clearControls(View vw) {
-
-        EditText ed_UserId = (EditText) findViewById(R.id.User_ID);
-        EditText ed_Pass = (EditText) findViewById(R.id.Pass);
-
-        ed_UserId.setText("");
-        ed_Pass.setText("");
-
-    }
-
-    public void postData(View vw) {
-
-        EditText ed_UserId = (EditText) findViewById(R.id.User_ID);
-        EditText ed_Pass = (EditText) findViewById(R.id.Pass);
-
-        String UserId = ed_UserId.getText().toString();
-        String Pass = ed_Pass.getText().toString();
-
-        if (UserId.equals("") || Pass.equals("")) {
-            Toast.makeText(this, "Please enter in all required fields.",
-                    Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        WebServiceTask wst = new WebServiceTask(WebServiceTask.POST_TASK, this, "Posting data...");
-
-        wst.addNameValuePair("firstName", UserId);
-        wst.addNameValuePair("lastName", Pass);
-
-        // the passed String is the URL we will POST to
-        wst.execute(new String[]{SERVICE_URL});
-
-    }
-
-    public void handleResponse(String response) {
-
-        EditText ed_UserId = (EditText) findViewById(R.id.User_ID);
-        EditText ed_Pass = (EditText) findViewById(R.id.Pass);
-
-        ed_UserId.setText("");
-        ed_Pass.setText("");
-
-        try {
-
-            JSONObject jso = new JSONObject(response);
-
-            String firstName = jso.getString("firstName");
-            String lastName = jso.getString("lastName");
-
-            ed_UserId.setText(firstName);
-            ed_Pass.setText(lastName);
-
-            } catch (Exception e) {
-            Log.e(TAG, e.getLocalizedMessage(), e);
-        }
-
-    }
-    private void hideKeyboard() {
-
-        InputMethodManager inputManager = (InputMethodManager) verificacionLogin.this
-                .getSystemService(Context.INPUT_METHOD_SERVICE);
-
-        inputManager.hideSoftInputFromWindow(
-                verificacionLogin.this.getCurrentFocus()
-                        .getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-    }
-
-    private class WebServiceTask extends AsyncTask<String, Integer, String> {
-
-        public static final int POST_TASK = 1;
-        public static final int GET_TASK = 2;
-
-        private static final String TAG = "WebServiceTask";
-
-        // connection timeout, in milliseconds (waiting to connect)
-        private static final int CONN_TIMEOUT = 3000;
-
-        // socket timeout, in milliseconds (waiting for data)
-        private static final int SOCKET_TIMEOUT = 5000;
-
-        private int taskType = GET_TASK;
-        private Context mContext = null;
-        private String processMessage = "Processing...";
-
-        private ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
-
-        private ProgressDialog pDlg = null;
-
-        public WebServiceTask(int taskType, Context mContext, String processMessage) {
-
-            this.taskType = taskType;
-            this.mContext = mContext;
-            this.processMessage = processMessage;
-        }
-
-        public void addNameValuePair(String name, String value) {
-
-            params.add(new BasicNameValuePair(name, value));
-        }
-
-        private void showProgressDialog() {
-
-            pDlg = new ProgressDialog(mContext);
-            pDlg.setMessage(processMessage);
-            pDlg.setProgressDrawable(mContext.getWallpaper());
-            pDlg.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            pDlg.setCancelable(false);
-            pDlg.show();
-
-        }
-
-        @Override
-        protected void onPreExecute() {
-
-            hideKeyboard();
-            showProgressDialog();
-
-        }
-
-        protected String doInBackground(String... urls) {
-
-            String url = urls[0];
-            String result = "";
-
-            HttpResponse response = doResponse(url);
-
-            if (response == null) {
-                return result;
-            } else {
-
-                try {
-
-                    result = inputStreamToString(response.getEntity().getContent());
-
-                } catch (IllegalStateException e) {
-                    Log.e(TAG, e.getLocalizedMessage(), e);
-
-                } catch (IOException e) {
-                    Log.e(TAG, e.getLocalizedMessage(), e);
-                }
-
-            }
-
-            return result;
-        }
-
-        @Override
-        protected void onPostExecute(String response) {
-
-            handleResponse(response);
-            pDlg.dismiss();
-
-        }
-
-        // Establish connection and socket (data retrieval) timeouts
-        private HttpParams getHttpParams() {
-
-            HttpParams htpp = new BasicHttpParams();
-
-            HttpConnectionParams.setConnectionTimeout(htpp, CONN_TIMEOUT);
-            HttpConnectionParams.setSoTimeout(htpp, SOCKET_TIMEOUT);
-
-            return htpp;
-        }
-
-        private HttpResponse doResponse(String url) {
-
-            // Use our connection and data timeouts as parameters for our
-            // DefaultHttpClient
-            HttpClient httpclient = new DefaultHttpClient(getHttpParams());
-
-            HttpResponse response = null;
-
-            try {
-                switch (taskType) {
-
-                    case POST_TASK:
-                        HttpPost httppost = new HttpPost(url);
-                        // Add parameters
-                        httppost.setEntity(new UrlEncodedFormEntity(params));
-
-                        response = httpclient.execute(httppost);
-                        break;
-                    case GET_TASK:
-                        HttpGet httpget = new HttpGet(url);
-                        response = httpclient.execute(httpget);
-                        break;
-                }
-            } catch (Exception e) {
-
-                Log.e(TAG, e.getLocalizedMessage(), e);
-
-            }
-
-            return response;
-        }
-
-        private String inputStreamToString(InputStream is) {
-
-            String line = "";
-            StringBuilder total = new StringBuilder();
-
-            // Wrap a BufferedReader around the InputStream
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-
-            try {
-                // Read response until the end
-                while ((line = rd.readLine()) != null) {
-                    total.append(line);
-                }
-            } catch (IOException e) {
-                Log.e(TAG, e.getLocalizedMessage(), e);
-            }
-
-            // Return full string
-            return total.toString();
-        }
-
-    }
-
-    public final class SessionIdentifierGenerator {
-        private SecureRandom random = new SecureRandom();
-        public String nextSessionId() {
-            return new BigInteger(130, random).toString(32);
         }
     }
-*/
 }
+
+
